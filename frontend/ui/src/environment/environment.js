d185e81ae35aac292d1b1f595d993510324e97ed
steve-999 <steve.green1@gmail.com>
2021-08-29 14:46:52 +0100

    minor modification

--- frontend/ui/src/environment/environment.js
+++ frontend/ui/src/environment/environment.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'http://localhost:5000/api';
-//export const API_BASE_URL = 'https://fantasy-football-app-r383r.ondigitalocean.app/api';
+//export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://fantasy-football-app-r383r.ondigitalocean.app/api';
 
 export const LEAGUE_ID = '1230538';     // '1437093';
